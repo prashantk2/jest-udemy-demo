deea824bc7804299b68c328518e531694b95b617
Prashant Kumar <prashantk2@dewsolutions.in>
2020-04-07 17:32:10 +0530

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React,{Component} from 'react';
 import './App.css';
-import Counter from './components/Counter/Counter';
+// import Counter from './components/Counter/Counter';
+import Congrats from './Congrats';
+import GuessedWord from './GuessedWord';
 
 class App extends Component {
   render(){
     return (
-      <div data-test="component-app">
-      <Counter />
-        
+      <div data-test="component-app" className="container">
+      <h1>Jotto App</h1>
+          <Congrats success={true} />
+          <GuessedWord guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]}  />
       </div>
     );
   }
--- src/Congrats.js
+++ src/Congrats.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Congrats = (props) => {
     if(props.success) {
         return (
-            <div data-test="component-congrats">
+            <div className="alert alert-success" data-test="component-congrats">
                 <span data-test="congrats-message">
                     Congrats! You guessed right.
                 </span>
