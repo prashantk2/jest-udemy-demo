1d6f09bd365caa8f2646021d6a00e218c8949b60
Prashant Kumar <prashantk2@dewsolutions.in>
2020-04-16 19:23:38 +0530

    connected app component and tested it

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React,{Component} from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 // import Counter from './components/Counter/Counter';
 import Congrats from './Congrats';
 import GuessedWord from './GuessedWord';
+import {getSecretWord} from './actions';
+import Input from './Input'
 
 class App extends Component {
   render(){
     return (
       <div data-test="component-app" className="container">
       <h1>Jotto App</h1>
-          <Congrats success={true} />
-          <GuessedWord guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]}  />
+          <Congrats success={this.props.success} />
+          <Input />
+          <GuessedWord guessedWords={this.props.guessedWords}  />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+}
+
+export default connect(mapStateToProps, {getSecretWord})(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,90 +1,36 @@
 import React from 'react';
-// // import { render } from '@testing-library/react';
+import {shallow} from 'enzyme';
+import {storeFactory} from '../test/testUtils';
+import App from './App';
 
-import Enzyme, {shallow} from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-// import App from './App'; 
+const setup = (state={}) => {
+    const store = storeFactory(state);
+    const wrapper = shallow(<App  store={store} />).dive().dive();
+    return wrapper;
+}
 
-Enzyme.configure({adapter: new EnzymeAdapter() });
-
-test('check if app rendered', () => {
-
-})
-
-
-// /**
-//  * Factory function to create shallowComponent for the App component
-//  * @function setup
-//  * @param {object} props -Props specific to this component
-//  * @param {any} state - Initial state of the setup
-//  * @returns {ShallowWrapper}
-//  */
-// const setup = (props={}, state=null) => {
-//   const wrapper = shallow(<App {...props} />);
-//   if(state) wrapper.setState(state);
-//   return wrapper;
-// }
-
-// /**
-//  * Return ShallowWrapper containing node(s) with given data-test value
-//  * @function findBtTestAttr
-//  * @param {ShallowComponent} wrapper -Enzyme wrapper to search within
-//  * @param {string} val -Value of data-test attribute for search
-//  * @returns {ShallowComponent} 
-//  */
-// const findByTestAttr = (wrapper, val) => {
-//   return wrapper.find(`[data-test="${val}"]`)
-// }
-
-// test('renders without error', () => {
-//   const wrapper = setup();
-//   const appComponent = findByTestAttr(wrapper, 'component-app');
-//   expect(appComponent.length).toBe(1);
-// });
-
-// test('renders increment button', () => {
-//   const wrapper = setup();
-//   const button = findByTestAttr(wrapper, 'increment-button');
-//   expect(button.length).toBe(1);
-// });
-
-// test('renders counter display', () => {
-//   const wrapper = setup();
-//   const counterDisplay = findByTestAttr(wrapper, 'counter-display');
-//   expect(counterDisplay.length).toBe(1);
-// });
-
-// test('Counter starts at 0', () => {
-//   const wrapper = setup();
-//   const initialCounterState = wrapper.state('counter');
-//   expect(initialCounterState).toBe(0);
-// });
-
-// test('clicking button increments the counter display', () => {
-//     const counter = 7;
-//     const wrapper = setup(null, {counter});
-//     const button = findByTestAttr(wrapper, 'increment-button');
-//     button.simulate('click');
-
-//     const counterDisplay = findByTestAttr(wrapper, 'counter-display');
-//     expect(counterDisplay.text()).toContain(counter + 1)
-// });
-
-// /**
-//  * Test cases for decrement counter
-//  */
-// test('render decrement button', () => {
-//   const wrapper = setup();
-//   const button = findByTestAttr(wrapper, 'decrement-button');
-//   expect(button.length).toBe(1);
-// });
-
-// test('clicking button decrement the counter', () => {
-//   const counter = 7;
-//   const wrapper = setup(null, {counter});
-//   const button = findByTestAttr(wrapper, 'decrement-button');
-//   button.simulate('click');
-
-//   const counterDisplay = findByTestAttr(wrapper, 'counter-display');
-//   expect(counterDisplay.text()).toContain(counter - 1);
-// })
+describe('redux properties',() => {
+    test('has access to the success state', () => {
+        const success = true;
+        const wrapper = setup({success});
+        const successProp = wrapper.instance().props.success;
+        expect(successProp).toBe(success);
+    });
+    test('has access to secretWord state', () => {
+        const secretWord = 'party';
+        const wrapper = setup({secretWord});
+        const secretWordProp = wrapper.instance().props.secretWord;
+        expect(secretWordProp).toBe(secretWord);
+    });
+    test('has access to the guessedWord state', () => {
+        const guessedWords = [{guessedWord: 'train',letterMatchCount: 3}];
+        const wrapper = setup({guessedWords});
+        const guessedWordsProp = wrapper.instance().props.guessedWords;
+        expect(guessedWordsProp).toBe(guessedWords);
+    });
+    test('getSecretWord action creator is a function on the props', () => {
+        const wrapper = setup();
+        const getSecretWordProp = wrapper.instance().props.getSecretWord;
+        expect(getSecretWordProp).toBeInstanceOf(Function);
+    });
+});
